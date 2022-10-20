2ca47d08c221fbe6da3cc6dc8d627efc46447e53
mtrigoboff <mtrigoboff@comcast.net>
2022-10-20 16:38:56 -0700

    added commas to numbers

--- table/table.py
+++ table/table.py
@@ -2,6 +2,7 @@
 #    < means left justify
 #    ^ means center
 #    > means right justify
+#    , means add commas to numbers
 
 hdrs = [['track',			'price',		'currency',		'total',	'rating'],
 		['-----',			'-----',		'--------',		'-----',	'------']
@@ -14,8 +15,8 @@ table = [['Facebook',		0.0,			'USD',			2974676,	3.5],
 		]
 
 for hdr in hdrs:
-	print(f'{hdr[0]:16s}{hdr[1]:>11s}{hdr[2]:^14s}{hdr[3]:>8s}{hdr[4]:>10s}')
+	print(f'{hdr[0]:16s}{hdr[1]:>11s}{hdr[2]:^14s}{hdr[3]:>12s}{hdr[4]:>10s}')
 for row in table:
-	print(f'{row[0]:16s}{row[1]:11.2f}{row[2]:^14s}{row[3]:8d}{row[4]:10.1f}')
+	print(f'{row[0]:16s}{row[1]:11.2f}{row[2]:^14s}{row[3]:>12,}{row[4]:10.1f}')
 
 print()
